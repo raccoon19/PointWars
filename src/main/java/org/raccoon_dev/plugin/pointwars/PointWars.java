932bbb8efe324af9e9096ecd4988df1576394936
Marcel Holstein <marcel-holstein@gmx.net>
2016-01-31 21:28:51 +0100

    add override functions

--- src/main/java/org/raccoon_dev/plugin/pointwars/PointWars.java
+++ src/main/java/org/raccoon_dev/plugin/pointwars/PointWars.java
@@ -1,9 +1,27 @@
 package org.raccoon_dev.plugin.pointwars;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by Marcel on 31.01.16.
  */
 public class PointWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+
 }
